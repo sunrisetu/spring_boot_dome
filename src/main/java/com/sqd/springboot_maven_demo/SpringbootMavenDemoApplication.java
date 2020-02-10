b2df7b417a028ed9b11517efcef62addc8a65267
shaoqidong <907722435@qq.com>
2020-02-10 13:51:37 +0800

    TEST

--- src/main/java/com/sqd/springboot_maven_demo/SpringbootMavenDemoApplication.java
+++ src/main/java/com/sqd/springboot_maven_demo/SpringbootMavenDemoApplication.java
@@ -1,7 +1,11 @@
 package com.sqd.springboot_maven_demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 
 @SpringBootApplication
 public class SpringbootMavenDemoApplication {
