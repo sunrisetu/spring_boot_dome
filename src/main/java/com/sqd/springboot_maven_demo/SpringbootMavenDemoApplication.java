2d80f9a352dc076f1818dc3e22f28296bef02bce
shaoqidong <907722435@qq.com>
2020-02-10 13:28:18 +0800

    init project

---
+++ src/main/java/com/sqd/springboot_maven_demo/SpringbootMavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sqd.springboot_maven_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMavenDemoApplication.class, args);
+    }
+
+}
